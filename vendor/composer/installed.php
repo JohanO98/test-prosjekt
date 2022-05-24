36831ff41249fda56a5a9f9cd59c11caba5fd561
Jonrongg <Johan_oen@hotmail.com>
2022-05-24 12:50:55 +0200

    Did 'composer install' command

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5b942926516a07b81ce9221d8f6d369f0b5b31d3',
+        'name' => 'johanoen98/skeleton',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'johanoen98/skeleton' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5b942926516a07b81ce9221d8f6d369f0b5b31d3',
+            'dev_requirement' => false,
+        ),
+    ),
+);
