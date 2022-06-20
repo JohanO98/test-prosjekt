f03ffb04c695ff1878be2d703b73cd5a188a86e1
JohanO98 <92085212+JohanO98@users.noreply.github.com>
2022-06-20 12:47:24 +0200

    removed some stuff

--- public/index.php
+++ public/index.php
@@ -1,25 +1,6 @@
 
 <?php
 
-$route = $_SERVER['REQUEST_URI'];
-
-switch ($route) {
-    case '/' :
-        require __DIR__ . "/../templates/home.html";
-        break;
-
-    case '' :
-        require __DIR__ . '/../templates/home.html';
-        break;
-
-    case '/about':
-        require __DIR__ . '/../templates/about.html';
-        break;
-    default:
-        http_response_code(404);
-        require __DIR__ . '/../templates/error.html';
-        break;
-
-}
+declare(strict_types=1)
 
 ?>
